99a156e12ea3961157814101a2813f0a06c2f17e
Alireza <a.razinejad@gmail.com>
2023-06-05 16:09:13 +0330

    Create the Regular expression example

---
+++ Python/For AI and Machine Learning/projects/6_10 Intermediate/9_Regular Expression/app.py
@@ -0,0 +1,20 @@
+import re
+
+
+def validate_ir_phone(number):
+    pattern = r"[09]{2}[1,3,2]{1}[0-9]{8}"
+
+    try:
+        print(f"Find All: {re.findall(pattern, number)}")
+        print(f"Sub it: {re.sub(pattern, 'REVERT', number)}")
+        match = re.search(pattern, number)
+        if match:
+            print(f"Match: {match.group()}")
+            return "Valid Number"
+    except Exception as error:
+        print(error)
+
+    return "Invalid Number"
+
+
+print(validate_ir_phone(input("Please enter your phone number: ")))
